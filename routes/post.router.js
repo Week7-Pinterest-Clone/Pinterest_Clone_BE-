fd8053d841fc64b10ee0979a6221c9269eb67c1e
wonsun choi <wonsunny@kakao.com>
2022-10-29 00:06:59 +0900

    라우터만드는중

--- routes/post.router.js
+++ routes/post.router.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const indexRouter = Router();
+const indexRouter = express.Router();
+
+const PostsController = require('../controllers/posts.controller');
+const postsController = new PostsController();
 
 //게시글업로드
 //게시글 이미지 업로드
@@ -8,5 +11,6 @@ const indexRouter = Router();
 //게시글 삭제
 //게시글 저장 (찜하기)
 
+router.get('/', postsController.getPosts);
 
 module.exports = router;
