985d3b3583589c2ad986ef8a0c555c9687b84739
jeonghyeon <kjhhy1005@gmail.com>
2022-10-29 02:50:34 +0900

    댓글 수정 레포 개발

--- repositories/comments.repository.js
+++ repositories/comments.repository.js
@@ -1,33 +1,44 @@
-const {Comment}  = require('../models/');
+const { Comment } = require("../models/");
 
 class CommentRepository extends Comment {
-    constructor() {
-        super()
-        }
-        //*댓글작성
-    createComment = async ({comment, postId, userId}) => {
-        
-        const createComment = await Comment.create({
-            comment,
-            userId,
-            postId
-        })
-        return createComment
-    }
-    //*댓글조회
-    findAllComment = async ({postId}) =>{
-        const findAllComment = await Comment.findAll({
-            where: {postId},
-            include: [{
-                model: User,
-                attributes:[nickname,userId]
+  constructor() {
+    super();
+  }
+  //*댓글작성
+  createComment = async ({ comment, postId, userId }) => {
+    const createComment = await Comment.create({
+      comment,
+      userId,
+      postId,
+    });
+    return createComment;
+  };
+  //*댓글 찾기
+  findOneComment = async (commentId) => {
+    const comments = await Comment.findByPk(commentId);
+    return comments;
+  };
+  //*댓글조회
+  findAllComment = async ({ postId }) => {
+    const findAllComment = await Comment.findAll({
+      where: { postId },
+      include: [
+        {
+          model: User,
+          attributes: [nickname, userId],
         },
-        {model:Like,
-        attributes:[isLike]}]
-        })
-        return findAllComment
-    }
-
-
+        { model: Like, attributes: [isLike] },
+      ],
+    });
+    return findAllComment;
+  };
+  //*댓글수정
+  updateComment = async ({ userId, comment, commentId }) => {
+    const updateComment = await Comment.update(
+      { comment: comment },
+      { where: { commentId: commentId, userId: userId } }
+    );
+    return updateComment;
+  };
 }
-module.exports = CommentRepository
+module.exports = CommentRepository;
