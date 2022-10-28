b9cd4ac1089a252ec78ff814bcb7910aad093eea
jeonghyeon <kjhhy1005@gmail.com>
2022-10-29 02:24:26 +0900

    댓글 목록 조회 레포 개발

--- repositories/comments.repository.js
+++ repositories/comments.repository.js
@@ -4,6 +4,7 @@ class CommentRepository extends Comment {
     constructor() {
         super()
         }
+        //*댓글작성
     createComment = async ({comment, postId, userId}) => {
         
         const createComment = await Comment.create({
@@ -13,6 +14,20 @@ class CommentRepository extends Comment {
         })
         return createComment
     }
+    //*댓글조회
+    findAllComment = async ({postId}) =>{
+        const findAllComment = await Comment.findAll({
+            where: {postId},
+            include: [{
+                model: User,
+                attributes:[nickname,userId]
+        },
+        {model:Like,
+        attributes:[isLike]}]
+        })
+        return findAllComment
+    }
+
 
 }
 module.exports = CommentRepository
