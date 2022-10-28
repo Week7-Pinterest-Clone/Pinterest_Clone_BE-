da9b742a1d0f42df2b2364ccddc50ba63a3a10f1
jeonghyeon <kjhhy1005@gmail.com>
2022-10-29 02:23:56 +0900

    댓글 목록 조회 서비스 개발

--- services/comments.service.js
+++ services/comments.service.js
@@ -1,12 +1,22 @@
-const CommentRepository = require("../repositories/comments.repository")
+const CommentRepository = require("../repositories/comments.repository");
 
 class CommentService {
-    constructor() {
-        this.commentRepository = new CommentRepository
-    }
-    createComment = async({comment,postId,userId}) =>{
-        const createComment = await this.commentRepository({comment,postId,userId})
-        return createComment
-    }
+  constructor() {
+    this.commentRepository = new CommentRepository();
+  }
+  //*댓글작성
+  createComment = async ({ comment, postId, userId }) => {
+    const createComment = await this.commentRepository.createComment({
+      comment,
+      postId,
+      userId,
+    });
+    return createComment;
+  };
+  //*댓글조회
+  findAllComment = async ({ postId }) => {
+    const findAllcomment = this.commentRepository.findAllComment({ postId });
+    return findAllcomment;
+  };
 }
-module.exports = CommentService
+module.exports = CommentService;
