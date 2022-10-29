dcbb1c38e2b12f3192de65a4b713876517b43ab8
jeonghyeon <kjhhy1005@gmail.com>
2022-10-30 05:13:59 +0900

    라이크 컨트롤러 수정

--- controllers/likes.controller.js
+++ controllers/likes.controller.js
@@ -1,10 +1,10 @@
 const LikeService = require("../services/likes.service");
 const InvalidParamsError = require("../exceptions/index.exception");
+const like = require("../models/like");
 
 class LikeController {
-  constructor() {
-    likeService = new LikeService();
-  }
+  likeService = new LikeService();
+
   liketoggle = async (req, res, next) => {
     try {
       const { userId } = res.locals.user;
@@ -18,23 +18,24 @@ class LikeController {
           userId,
           commentId,
         });
-        res.status(201).json({
-          msg: "좋아요 등록완료",
-        });
+        const likeCount = await this.likeService.likeCount({ commentId });
+        return res
+          .status(201)
+          .json({ data: likeCount, msg: "좋아요 등록완료" });
       }
       if (findLike) {
         const destroyLike = await this.likeService.destroyLike({
           userId,
           commentId,
         });
-        res.status(200).json({
-          msg: "좋아요 등록취소",
-        });
+        const likeCount = await this.likeService.likeCount({ commentId });
+        return res
+          .status(200)
+          .json({ data: likeCount, msg: "좋아요 등록취소" });
       }
-      const likeCount = await this.likeService.likeCount({ commentId });
-      res.status(200).json({ data: likeCount, msg: "좋아요 갯수" });
     } catch (error) {
       next(error);
     }
   };
 }
+module.exports = LikeController;
