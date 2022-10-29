da11509d3b17803ab1b6e07653eab9d8ec36e58b
Lsp <spyro9282@gmail.com>
2022-10-29 10:50:04 +0900

    프로필조회

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -7,7 +7,7 @@ const loginAuthMiddleware = require("../middlewares/loginAuthMiddleware");
 const UsersController = require("../controllers/users.controller");
 const usersController = new UsersController();
 
-// usersRouter.get("/:userId", authMiddleware, usersController.profile);
+usersRouter.get("/:userId", authMiddleware, usersController.profile);
 usersRouter.post("/signup", loginAuthMiddleware, usersController.signup);
 usersRouter.post("/login", loginAuthMiddleware, usersController.login);
 
