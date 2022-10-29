54e2acf20662f129043ec6b0a93987140839607a
jeonghyeon <kjhhy1005@gmail.com>
2022-10-29 15:00:38 +0900

    좋아요 데이터 찾기 레포 개발

--- repositories/likes.repository.js
+++ repositories/likes.repository.js
@@ -5,12 +5,17 @@ class LikeRepository extends Like {
   constructor() {
     super();
   }
+  //*좋아요 데이터 찾기
+  findLike = async ({ userId, commentId }) => {
+    const findLike = await Like.findOne({ where: { userId, commentId } });
+    return findLike;
+  };
   //*좋아요 만들기
   createLike = async ({ userId, commentId, isLike }) => {
     const createLike = await Like.create({ userId, commentId, isLike });
     return createLike;
   };
-  //*좋아요 on/off
+  //*좋아요 on
   upLike = async ({ commentId }) => {
     const upLike = await Comment.increment(
       { likeCount: 1 },
@@ -18,6 +23,7 @@ class LikeRepository extends Like {
     );
     return upLike;
   };
+  //*좋아요off
   downLike = async ({ commentId }) => {
     const downLike = await Comment.increment(
       { likeCount: -1 },
@@ -25,4 +31,6 @@ class LikeRepository extends Like {
     );
     return downLike;
   };
+  likeCount = async({});
 }
+module.exports = LikeRepository;
