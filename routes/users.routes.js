9f1f43cc709950197f4afb7e77b85d77bf7849b5
wonsun choi <wonsunny@kakao.com>
2022-10-31 23:31:56 +0900

    프로필이미지경로변경

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,35 +1,35 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const usersRouter = Router();
-const upload = require("../modules/users.multer");
+const upload = require('../modules/users.multer');
 
-const authMiddleware = require("../middlewares/authMiddleware");
-const loginAuthMiddleware = require("../middlewares/loginAuthMiddleware");
+const authMiddleware = require('../middlewares/authMiddleware');
+const loginAuthMiddleware = require('../middlewares/loginAuthMiddleware');
 
-const UsersController = require("../controllers/users.controller");
+const UsersController = require('../controllers/users.controller');
 const usersController = new UsersController();
-const ImagesController = require("../controllers/images.controller");
+const ImagesController = require('../controllers/images.controller');
 const imagesController = new ImagesController();
 
 //회원가입
-usersRouter.post("/signup", loginAuthMiddleware, usersController.signup);
+usersRouter.post('/signup', loginAuthMiddleware, usersController.signup);
 
 //로그인
-usersRouter.post("/login", loginAuthMiddleware, usersController.login);
+usersRouter.post('/login', loginAuthMiddleware, usersController.login);
 
 //유저 프로필 조회
-usersRouter.get("/:userId", authMiddleware, usersController.profile);
+usersRouter.get('/:userId', authMiddleware, usersController.profile);
 
 //프로필 이미지
 usersRouter.put(
-  "/:userId/image",
+  '/profile/:userId',
   authMiddleware,
-  upload.array("image", 1),
+  upload.array('image', 1),
   imagesController.uploadImage
 );
 
 //내 정보 수정 페이지
 usersRouter.get(
-  "/profile/:userId",
+  '/profile/:userId',
   authMiddleware,
   usersController.profileUpdate
 );
