2e3d14fa0d041f4e12b23e49ac8976ddb6d08b31
Lsp <spyro9282@gmail.com>
2022-10-31 17:05:38 +0900

    https수정

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const {
 } = require("./middlewares/error-handler.middleware");
 
 const app = express();
-const http = Http.createServer(app);
+// const http = Http.createServer(app);
 const port = process.env.EXPRESS_PORT || 3000;
 
 app.use(cors());
@@ -26,29 +26,28 @@ app.use("/", indexRouter);
 app.use(errorLogger); // Error Logger
 app.use(errorHandler); // Error Handler
 
-// if (process.env.NODE_ENV == "production") {
-//   try {
-//     const option = {
-//       ca: fs.readFileSync("/etc/letsencrypt/live/pyo00.shop/fullchain.pem"),
-//       key: fs.readFileSync("/etc/letsencrypt/live/pyo00.shop/privkey.pem"),
-//       cert: fs.readFileSync("/etc/letsencrypt/live/pyo00.shop/cert.pem"),
-//     };
+if (process.env.NODE_ENV == "production") {
+  try {
+    const option = {
+      ca: fs.readFileSync("/etc/letsencrypt/live/pyo00.shop/fullchain.pem"),
+      key: fs.readFileSync("/etc/letsencrypt/live/pyo00.shop/privkey.pem"),
+      cert: fs.readFileSync("/etc/letsencrypt/live/pyo00.shop/cert.pem"),
+    };
+    HTTPS.createServer(option, app).listen(port, () => {
+      console.log("HTTPS 서버가 실행되었습니다. 포트 :: " + port);
+    });
+  } catch (error) {
+    console.log("HTTPS 서버가 실행되지 않습니다.");
+    console.log(error);
+  }
+} else {
+  app.listen(port, () => {
+    console.log("HTTP 서버가 실행되었습니다. 포트 :: " + port);
+  });
+}
 
-//     HTTPS.createServer(option, app).listen(port, () => {
-//       console.log("HTTPS 서버가 실행되었습니다. 포트 :: " + port);
-//     });
-//   } catch (error) {
-//     console.log("HTTPS 서버가 실행되지 않습니다.");
-//     console.log(error);
-//   }
-// } else {
-//   app.listen(port, () => {
-//     console.log("HTTP 서버가 실행되었습니다. 포트 :: " + port);
-//   });
-// }
+// http.listen(port, () => {
+//   console.log(`Start listen Server: ${port}`);
+// });
 
-http.listen(port, () => {
-  console.log(`Start listen Server: ${port}`);
-});
-
-module.exports = http;
+// module.exports = http;
